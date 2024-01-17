e018d8dd68113db81d4505ac62be63b9734149ea
glowacz <glowacki.pj@gmail.com>
2024-01-17 22:27:03 +0100

    gitignore

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff4668df0656306af57517c4e0f6df0ae2ae4f7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+636191d90516c3284a5694042797c55335623e15")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
